93e15725b90c483658dd66713fb16724d7413061
Velkee <89312879+Velkee@users.noreply.github.com>
2022-11-09 12:33:12 +0100

    Express API init

---
+++ api/index.ts
@@ -0,0 +1,10 @@
+import express, { request, response } from 'express';
+const app = express();
+
+app.get('/', (_request, response) => {
+	response.send('API is online: hello world!');
+});
+
+app.listen(8000, () => {
+	console.log('API running on port 8000');
+});
