06734fe6bd623e1ac0dba4a30ebde235568fed6d
Fenic <89312879+Velkee@users.noreply.github.com>
2022-12-15 22:57:40 +0100

    API requiest recieved

--- api/src/main.ts
+++ api/src/main.ts
@@ -2,8 +2,15 @@ import express from 'express';
 const app = express();
 const port = 23450;
 
-app.post('/send')
+app.use("/send", express.urlencoded({ extended: true }));
+
+
+app.post('/send', (req, res) => {
+    const body = (req.body)
+    console.log(body)
+    res.redirect('http://localhost:8080')
+})
 
 app.listen(port, () => {
-    console.log('API running on port $(port)');
+    console.log(`API running on port ${port}`);
 })
