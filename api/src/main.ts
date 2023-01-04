bd1ab7e873eb4754bb9ccc57fa734184b777fa9a
Velkee <89312879+Velkee@users.noreply.github.com>
2023-01-04 14:12:15 +0100

    Starting work on image uploads

--- api/src/main.ts
+++ api/src/main.ts
@@ -1,16 +1,28 @@
 import express from 'express';
+import multer from 'multer';
 const app = express();
 const port = 23450;
 
-app.use("/send", express.urlencoded({ extended: true }));
+const storage = multer.diskStorage({
+	destination: function (req, file, cb) {
+		cb(null, '/tmp/uploads');
+	},
+	filename: function (req, file, cb) {
+		const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1e9);
+		cb(null, file.fieldname + '-' + uniqueSuffix);
+	},
+});
 
+const upload = multer({ storage: storage });
+
+app.use('/send', express.urlencoded({ extended: true }));
 
 app.post('/send', (req, res) => {
-    const body = (req.body)
-    console.log(body)
-    res.redirect('http://localhost:8080')
-})
+	const body = req.body;
+	console.log(body);
+	res.redirect('http://localhost:8080');
+});
 
 app.listen(port, () => {
-    console.log(`API running on port ${port}`);
-})
+	console.log(`API running on port ${port}`);
+});
--- frontend/src/routes/submit/+page.svelte
+++ frontend/src/routes/submit/+page.svelte
@@ -3,7 +3,7 @@
 </header>
 
 <main class="main-content">
-    <form action="http://localhost:23450/send" method="post">
+    <form action="http://localhost:23450/send" method="post" enctype="multipart/form-data">
         <label for="buissenessName">Buisseness name (case insensitive):</label>
         <br />
         <input type="text" name="buissenessName" id="buissenessName">
@@ -21,7 +21,7 @@
 
         <label for="imageUpload">Please upload an image so we can verify the request:</label>
         <br />
-        <input type="file" name="imageUpload" id="imageUpload" accept=".png,.jpg,gif">
+        <input type="file" name="imageUpload" id="imageUpload" accept=".jpg,.png">
         <br />
         <button type="submit">Submit</button>
     </form>
