9da9d0d9299ebc8dbcdb9970530682ad283c2b4e
Fenic <89312879+Velkee@users.noreply.github.com>
2023-02-01 10:57:38 +0100

    API can now connect to the backend

--- api/src/main.ts
+++ api/src/main.ts
@@ -1,21 +1,27 @@
 import express from 'express';
 import multer from 'multer';
 import dotenv from 'dotenv';
+import pg from 'pg';
 
 dotenv.config();
 
-const app = express();
 const api_port = process.env.API_PORT;
 const webserver_ip = process.env.WEBSERVER_IP;
 const webserver_port = process.env.WEBSERVER_PORT;
+
+const app = express();
+
+const client = new pg.Client();
+await client.connect();
+
 app.use('/send', express.urlencoded({ extended: true }));
 
 // Sets up multer's storage location
 const storage = multer.diskStorage({
-	destination: function (req, file, cb) {
+	destination: function (_req, _file, cb) {
 		cb(null, './tmp/uploads');
 	},
-	filename: function (req, file, cb) {
+	filename: function (_req, file, cb) {
 		const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1e9);
 		cb(null, file.fieldname + '-' + uniqueSuffix);
 	},
